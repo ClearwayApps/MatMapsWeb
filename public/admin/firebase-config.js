6e311ffd8c420c3af7ac609181f61e1f6e21ed9c
viswajit Roy <royviswajit@ymail.com>
2026-08-23 11:09:22 +0600

    fix: hide admin login from homepage and fix live config crash

---
+++ public/admin/firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  "apiKey": "AIzaSyAVp1TmwmsaapC3rBtsWVBYbmo3-uFxOEs",
+  "authDomain": "matmaps-1f5b2.firebaseapp.com",
+  "projectId": "matmaps-1f5b2",
+  "storageBucket": "matmaps-1f5b2.firebasestorage.app",
+  "messagingSenderId": "735375898638",
+  "appId": "1:735375898638:web:3aed1fde4aa7b0a95ae45f",
+  "measurementId": "G-TEBMGDJZGF"
+};
